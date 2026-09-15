cb16dca24ae00e564329e84241ec443c755912fc
Milan Mrdenovic <milan.mrdenovic@ibm.com>
2026-09-15 11:13:12 +0200

    Updated version of the config generator

--- softwarehub_cp4d_vars_generation.py
+++ softwarehub_cp4d_vars_generation.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.23.8"
+__generated_with = "0.24.0"
 app = marimo.App(
     width="full",
     app_title="Cloud Pak For Data - Setup Config Generator",
@@ -205,8 +205,9 @@ def _():
         "entitlements_multiselect": "**Select all of the license entitlements you wish to apply:**",
         "optional_images_multiselect": "**Select any optional models or images you wish to mirror:**",
         "prod_license": "Set licenses to **production** version?",
-        "updating_components": "Are you updating **existing** components?",
+        "updating_components": "Are you updating **existing** components? **DO NOT** Select if installing a new instance.",
         "cp4d_version": "**Cloud Pak for Data version:**",
+        "cp4d_patch": "**Patch ID (optional):**",
         "entitlement_key": "**Input your ibm entitlement key:** *[Get one here](https://myibm.ibm.com/products-services/containerlibrary)*",
         "cluster_url": "**Enter your openshift cluster url:**",
         "cluster_token": "**Enter your openshift token:**",
@@ -224,6 +225,8 @@ def _(
     block_storage_class_select,
     cluster_arch_select,
     cluster_type_select,
+    cp4d_patch_input,
+    cp4d_version_input,
     file_storage_class_select,
     image_pull_creds_select,
     login_argument_select,
@@ -249,6 +252,13 @@ def _(
                 [
                     image_pull_creds_select.style({"width": widget_width}),
                     file_storage_class_select.style({"width": widget_width}),
+                ],
+                justify="space-around",
+            ),
+            mo.hstack(
+                [
+                    cp4d_version_input.style({"width": widget_width}),
+                    cp4d_patch_input.style({"width": widget_width}),
                 ],
                 justify="space-around",
             ),
@@ -286,15 +296,29 @@ def _(
 def _(
     component_selection_tables,
     license_entitlement_table,
+    licenses_are_prod_checkbox,
     optional_images_selection_tables,
     prepare_inst_options_stack,
+    updating_components_checkbox,
 ):
     component_specs_accordion = mo.accordion(
         items={
-            "**License Entitlements**": license_entitlement_table.center(),
-            "**Components to Install**": component_selection_tables.style(
-                {"width": "60%"}
+            "**License Entitlements**": mo.vstack(
+                [
+                    license_entitlement_table,
+                    licenses_are_prod_checkbox.center(),
+                ],
+                gap=1,
             ).center(),
+            "**Components to Install**": mo.vstack(
+                [
+                    component_selection_tables,
+                    updating_components_checkbox,
+                ],
+                gap=1,
+            )
+            .style({"width": "60%"})
+            .center(),
             "*Models & Images to Mirror* ***Optional***": optional_images_selection_tables.style(
                 {"width": "60%"}
             ).center(),
@@ -484,7 +508,7 @@ def _(cp4d_component_id_records, widget_labels):
 @app.cell
 def _(cp4d_component_id_records):
     install_component_table_2 = mo.ui.table(
-        label="Apply the appropriate license entitlements in the next table.",
+        label="Apply the appropriate license entitlements in the license table.",
         data=cp4d_component_id_records[(len(cp4d_component_id_records) // 2) :],
         selection="multi",
         page_size=60,
@@ -509,8 +533,7 @@ def _(install_component_table_1, install_component_table_2):
 def _(install_component_table_1, install_component_table_2):
     components_to_install = [
         item["component_id"]
-        for item in install_component_table_1.value
-        + install_component_table_2.value
+        for item in install_component_table_1.value + install_component_table_2.value
     ]
     print(components_to_install)
     components_to_install_str = ",".join(components_to_install)
@@ -629,9 +652,7 @@ def _(widget_labels):
     licenses_are_prod_checkbox = mo.ui.checkbox(
         label=widget_labels.get("prod_license"), value=True
     )
-
-    # licenses_are_prod_checkbox
-    return
+    return (licenses_are_prod_checkbox,)
 
 
 @app.cell
@@ -639,21 +660,26 @@ def _(widget_labels):
     updating_components_checkbox = mo.ui.checkbox(
         label=widget_labels.get("updating_components"), value=False
     )
-
-    # updating_components_checkbox
-    return
+    return (updating_components_checkbox,)
 
 
 @app.cell
 def _(widget_labels):
     cp4d_version_input = mo.ui.text(
         label=widget_labels.get("cp4d_version"),
-        value="5.3.1",
-        full_width=False,
+        value="5.4.0",
+        full_width=True,
     )
+    return (cp4d_version_input,)
 
-    # cp4d_version_input
-    return
+
+@app.cell
+def _(widget_labels):
+    cp4d_patch_input = mo.ui.text(
+        label=widget_labels.get("cp4d_patch"),
+        full_width=True,
+    )
+    return (cp4d_patch_input,)
 
 
 @app.cell
@@ -734,7 +760,7 @@ def _(widget_labels):
 def _(widget_labels):
     cpd_admin_username_input = mo.ui.text(
         label=widget_labels.get("cpd_admin_username"),
-        value="kubeadmin",
+        value="cpadmin",
         kind="text",
         full_width=False,
     )
